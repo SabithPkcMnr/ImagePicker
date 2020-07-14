43cc6f54023e67aade3d9d13d1a4a08f37763fb3
Sabith Pkc Mnr <business@sabithpkcmnr.com>
2020-07-15 02:23:19 +0530

    Project by Sabith Pkc Mnr

---
+++ app/src/main/java/com/sabithpkcmnr/gallerytoimageview/ActivityHome.java
@@ -0,0 +1,43 @@
+package com.sabithpkcmnr.gallerytoimageview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityHome extends AppCompatActivity {
+
+    Button myButton;
+    ImageView myImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        myImage = findViewById(R.id.myImage);
+        myButton = findViewById(R.id.myButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, 123);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123){
+            myImage.setImageURI(data.getData());
+        }
+    }
+}
